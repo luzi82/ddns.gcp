b9f7e2ee73d42ba030f5f66a8d7cfbaee1ddb89f
luzi82 <luzi82@gmail.com>
2020-03-01 19:47:57 +0800

    conf

---
+++ conf/upload.py
@@ -0,0 +1,13 @@
+import datetime
+import futsu.json
+import futsu.storage
+import os
+
+timestamp = int(datetime.datetime.now().timestamp())
+
+setting_data = futsu.json.path_to_data('setting.json')
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = setting_data['GOOGLE_APPLICATION_CREDENTIALS']
+
+futsu.storage.local_to_path(setting_data['SETTING_PATH'], 'setting.json')
+futsu.storage.local_to_path(setting_data['SETTING_BACKUP_PATH'].replace('{TIMESTAMP}',str(timestamp)), 'setting.json')
