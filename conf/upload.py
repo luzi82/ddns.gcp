dade5b9b99f8f34efdb7a37418cafc29764ed214
luzi82 <luzi82@gmail.com>
2024-05-26 21:12:29 +0800

    upload add project id

--- conf/upload.py
+++ conf/upload.py
@@ -8,6 +8,7 @@ timestamp = int(datetime.datetime.now().timestamp())
 setting_data = futsu.json.path_to_data('setting.json')
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = setting_data['GOOGLE_APPLICATION_CREDENTIALS']
+os.environ["GCLOUD_PROJECT"] = setting_data['PROJECT_ID']
 
 futsu.storage.local_to_path(setting_data['SETTING_PATH'], 'setting.json')
 futsu.storage.local_to_path(setting_data['SETTING_BACKUP_PATH'].replace('{TIMESTAMP}',str(timestamp)), 'setting.json')
